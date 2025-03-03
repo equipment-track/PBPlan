e52517172803b2b3a7d4ac96bf93a80b44615c82
equipment-track <nafil.8895.s@gmail.com>
2025-03-03 18:48:57 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,28 @@
-const CACHE_NAME = 'budget-pwa-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/transactions.html',
-    '/settings.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json'
+const cacheName = 'budget-calculator-v1';
+const assets = [
+    './',
+    './index.html',
+    './styles.css',
+    './script.js',
+    './manifest.json',
+    './icon-192x192.png',
+    './icon-512x512.png'
 ];
 
-// Install service worker
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(assets);
             })
     );
 });
 
-// Fetch data from cache
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then((cachedResponse) => {
-                return cachedResponse || fetch(event.request);
+            .then(response => {
+                return response || fetch(event.request);
             })
     );
 });
